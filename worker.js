404520c2315573ffc81ec6cf86e135d85e3516ea
Jared Forsyth <jared@jaredforsyth.com>
2013-10-10 15:49:58 -0600

    update

--- worker.js
+++ worker.js
@@ -2,18 +2,18 @@
 var git = require('strider-git/worker')
 
 module.exports = {
-  init: function (dest, account, config, job, done) {
+  init: function (dirs, account, config, job, done) {
     return done(null, {
       config: config,
       account: account,
       fetch: function (context, done) {
-        module.exports.fetch(dest, account, config, job, context, done)
+        module.exports.fetch(dirs.data, account, config, job, context, done)
       }
     })
   },
   fetch: function (dest, account, config, job, context, done) {
     if (config.scm !== 'git') {
-      return done(new Error('Bitbucket repo is not git... Other SCMs will be supported shortly.'))
+      return done(new Error('Bitbucket repo is not git... Mercurial will be supported shortly.'))
     }
     if (config.auth.type === 'https' && !config.auth.username) {
       config.auth.username = account.accessToken
